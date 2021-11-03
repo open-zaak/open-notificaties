83179f803030258a9d503b29e29898c943f5a549
silvia <silvia@maykinmedia.nl>
2021-11-03 09:15:19 +0100

    :sparkles: Conversion to cloud event

--- src/nrc/api/serializers.py
+++ src/nrc/api/serializers.py
@@ -1,6 +1,7 @@
+import json
 import logging
+from furl import furl
 
-from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from django.utils.translation import ugettext_lazy as _
@@ -14,6 +15,7 @@ from nrc.api.tasks import deliver_message
 from nrc.datamodel.models import Abonnement, Filter, FilterGroup, Kanaal, Notificatie
 
 from .validators import CallbackURLAuthValidator, CallbackURLValidator
+from ..config.models import CloudEventConfig
 
 logger = logging.getLogger(__name__)
 
@@ -183,16 +185,15 @@ class MessageSerializer(NotificatieSerializer):
             if group.match_pattern(msg_filters):
                 subs.add(group.abonnement)
 
-        task_kwargs = {}
-        if settings.LOG_NOTIFICATIONS_IN_DB:
-            # creation of the notification
-            kanaal = Kanaal.objects.get(naam=msg["kanaal"])
-            notificatie = Notificatie.objects.create(forwarded_msg=msg, kanaal=kanaal)
-            task_kwargs.update({"notificatie_id": notificatie.id})
+        # creation of the notification
+        kanaal = Kanaal.objects.get(naam=msg["kanaal"])
+        notificatie = Notificatie.objects.create(forwarded_msg=msg, kanaal=kanaal)
+
+        notification_content = self.convert_notification_to_cloudevent(notification_id=notificatie.id)
 
         # send to subs
         for sub in list(subs):
-            deliver_message.delay(sub.id, msg, **task_kwargs)
+            deliver_message.delay(sub.id, notification_content)
 
     def create(self, validated_data: dict) -> dict:
         logger.info("Handling notification %r", validated_data)
@@ -200,3 +201,24 @@ class MessageSerializer(NotificatieSerializer):
         # send to subs
         self._send_to_subs(validated_data)
         return validated_data
+
+    def convert_notification_to_cloudevent(self, notification_id):
+        config = CloudEventConfig.get_solo()
+        notification_content = self.validated_data
+        base_url = furl(self.context['request'].build_absolute_uri())
+
+        converted_content = {
+            "id": notification_id,
+            "type": f"nl.vng.zgw.{notification_content['kanaal']}.{notification_content['resource']}.{notification_content['actie']}",
+            "time": notification_content['aanmaakdatum'].strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            "source": f"urn:nld:oin:{config.oin}:systeem:{base_url.host}",
+            "data": json.dumps(self.initial_data),
+            "specversion": "1.0",
+            "datacontenttype": "application/json"
+        }
+        kenmerken = notification_content.get("kenmerken")
+        if kenmerken:
+            for key, value in kenmerken.items():
+                converted_content[f"nl.vng.zgw.{key}"] = value
+
+        return converted_content
