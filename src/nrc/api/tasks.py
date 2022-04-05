344315a0e71026bbc80f81153d74ea39ee370959
Steven Bal <steven@maykinmedia.nl>
2022-04-05 12:05:55 +0200

    :sparkles: [open-zaak/open-zaak#1132] Retry mechanism for notification delivery

--- src/nrc/api/tasks.py
+++ src/nrc/api/tasks.py
@@ -1,7 +1,9 @@
 import json
 import logging
 
+from django.conf import settings
 from django.core.serializers.json import DjangoJSONEncoder
+from django.utils.translation import gettext_lazy as _
 
 import requests
 
@@ -11,7 +13,19 @@ from nrc.datamodel.models import Abonnement, NotificatieResponse
 logger = logging.getLogger(__name__)
 
 
-@app.task(ignore_result=True)
+class NotificationException(Exception):
+    pass
+
+
+@app.task(
+    autoretry_for=(
+        NotificationException,
+        requests.exceptions.RequestException,
+    ),
+    max_retries=settings.CELERY_MAX_RETRIES,
+    retry_backoff=settings.CELERY_RETRY_BACKOFF,
+    retry_backoff_max=settings.CELERY_RETRY_BACKOFF_MAX,
+)
 def deliver_message(sub_id: int, msg: dict, **kwargs) -> None:
     """
     send msg to subscriber
@@ -35,16 +49,23 @@ def deliver_message(sub_id: int, msg: dict, **kwargs) -> None:
             headers={"Content-Type": "application/json", "Authorization": sub.auth},
         )
         response_init_kwargs = {"response_status": response.status_code}
+        if response.status_code != 204:
+            exception_message = _(
+                "Could not send notification: status {status_code} - {response}"
+            ).format(status_code=response.status_code, response=response.text)
+            response_init_kwargs["exception"] = exception_message
+            raise NotificationException(exception_message)
     except requests.exceptions.RequestException as e:
-        # log of the response of the call
         response_init_kwargs = {"exception": str(e)}
-
-    logger.debug(
-        "Notification response for %d, %r: %r", sub_id, msg, response_init_kwargs
-    )
-
-    # Only log if a top-level object is provided
-    if notificatie_id:
-        NotificatieResponse.objects.create(
-            notificatie_id=notificatie_id, abonnement=sub, **response_init_kwargs
+        raise e
+    finally:
+        # log of the response of the call
+        logger.debug(
+            "Notification response for %d, %r: %r", sub_id, msg, response_init_kwargs
         )
+
+        # Only log if a top-level object is provided
+        if notificatie_id:
+            NotificatieResponse.objects.create(
+                notificatie_id=notificatie_id, abonnement=sub, **response_init_kwargs
+            )
