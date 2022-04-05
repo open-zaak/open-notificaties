9326425e40093166f71ee7613fb2caff23873eb4
Steven Bal <steven@maykinmedia.nl>
2022-04-05 12:46:39 +0200

    :white_check_mark: [open-zaak/open-zaak#1132] Add/update tests for delivery retry

--- src/nrc/api/tests/test_tasks.py
+++ src/nrc/api/tests/test_tasks.py
@@ -1,7 +1,10 @@
 import json
+from unittest.mock import patch
 
 from django.core.serializers.json import DjangoJSONEncoder
+from django.utils.translation import gettext as _
 
+import celery
 import requests
 import requests_mock
 from rest_framework.test import APITestCase
@@ -37,7 +40,7 @@ class NotifCeleryTests(APITestCase):
         }
 
         with requests_mock.mock() as m:
-            m.post(abon.callback_url)
+            m.post(abon.callback_url, status_code=204)
 
             deliver_message(abon.id, msg, notificatie_id=notif.id)
 
@@ -71,7 +74,7 @@ class NotifCeleryTests(APITestCase):
         msg = json.dumps(request_data, cls=DjangoJSONEncoder)
 
         with requests_mock.mock() as m:
-            m.post(abon.callback_url, status_code=201)
+            m.post(abon.callback_url, status_code=204)
 
             deliver_message(abon.id, msg, notificatie_id=notif.id)
 
@@ -79,15 +82,61 @@ class NotifCeleryTests(APITestCase):
 
         notif_response = NotificatieResponse.objects.get()
 
-        self.assertEqual(notif_response.response_status, 201)
+        self.assertEqual(notif_response.response_status, 204)
         self.assertEqual(notif_response.exception, "")
 
-    def test_notificatie_log_exception(self):
+    @patch("nrc.api.tasks.deliver_message.retry")
+    def test_notificatie_invalid_response_retry(self, retry_mock):
         """
-        test /notificatie POST:
-        check if message was send to subscribers callbackUrls
+        Verify that a retry is called when the sending of the notification didn't
+        succeed due to an invalid response
+        """
+        retry_mock.side_effect = celery.exceptions.Retry
 
+        abon = AbonnementFactory.create()
+        notif = NotificatieFactory.create()
+
+        request_data = {
+            "kanaal": "zaken",
+            "hoofdObject": "https://ref.tst.vng.cloud/zrc/api/v1/zaken/d7a22",
+            "resource": "status",
+            "resourceUrl": "https://ref.tst.vng.cloud/zrc/api/v1/statussen/d7a22/721c9",
+            "actie": "create",
+            "aanmaakdatum": "2018-01-01T17:00:00Z",
+            "kenmerken": {
+                "bron": "082096752011",
+                "zaaktype": "example.com/api/v1/zaaktypen/5aa5c",
+                "vertrouwelijkheidaanduiding": "openbaar",
+            },
+        }
+        msg = json.dumps(request_data, cls=DjangoJSONEncoder)
+        error_msg = {"error": "Something went wrong"}
+
+        with requests_mock.mock() as m:
+            m.post(abon.callback_url, status_code=400, json=error_msg)
+
+            with self.assertRaises(celery.exceptions.Retry):
+                deliver_message(abon.id, msg, notificatie_id=notif.id)
+
+        self.assertEqual(NotificatieResponse.objects.count(), 1)
+
+        notif_response = NotificatieResponse.objects.get()
+
+        self.assertEqual(
+            notif_response.exception,
+            _("Could not send notification: status {status_code} - {response}").format(
+                status_code=400, response=json.dumps(error_msg)
+            ),
+        )
+
+    @patch("nrc.api.tasks.deliver_message.retry")
+    def test_notificatie_request_exception_retry(self, retry_mock):
         """
+        Verify that a retry is called when the sending of the notification didn't
+        succeed due to a request exception
+        """
+        retry_mock.side_effect = celery.exceptions.Retry
+
         abon = AbonnementFactory.create()
         notif = NotificatieFactory.create()
 
@@ -112,7 +161,8 @@ class NotifCeleryTests(APITestCase):
                 exc=requests.exceptions.ConnectTimeout("Timeout exception"),
             )
 
-            deliver_message(abon.id, msg, notificatie_id=notif.id)
+            with self.assertRaises(celery.exceptions.Retry):
+                deliver_message(abon.id, msg, notificatie_id=notif.id)
 
         self.assertEqual(NotificatieResponse.objects.count(), 1)
 
