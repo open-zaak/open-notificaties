4f8071c02d9b5a4211b69a153bd175af786db43c
Sergei Maertens <sergei@maykinmedia.nl>
2022-07-01 12:04:48 +0200

    :test_tube: Added regression test for OIDC session refresh crash

---
+++ src/nrc/tests/test_oidc_middleware.py
@@ -0,0 +1,38 @@
+from unittest.mock import patch
+from urllib.parse import urlparse
+
+from django.urls import reverse
+
+from django_webtest import WebTest
+from mozilla_django_oidc_db.models import OpenIDConnectConfig
+
+from nrc.accounts.tests.factories import SuperUserFactory
+
+
+class AdminSessionRefreshMiddlewareTests(WebTest):
+    @patch(
+        "mozilla_django_oidc_db.mixins.OpenIDConnectConfig.get_solo",
+        return_value=OpenIDConnectConfig(
+            enabled=True,
+            oidc_op_authorization_endpoint="https://example.com/auth/",
+        ),
+    )
+    @patch(
+        "mozilla_django_oidc.middleware.SessionRefresh.is_refreshable_url",
+        return_value=True,
+    )
+    def test_session_refresh_no_crash(self, *mocks):
+        """
+        Regression test for crash on admin login because of session refresh.
+        """
+        user = SuperUserFactory.create()
+        admin_url = reverse("admin:index")
+
+        response = self.app.get(admin_url, user=user)
+
+        # we are being redirected to OIDC
+        self.assertEqual(response.status_code, 302)
+        redirect_url = urlparse(response["Location"])
+        self.assertEqual(redirect_url.scheme, "https")
+        self.assertEqual(redirect_url.netloc, "example.com")
+        self.assertEqual(redirect_url.path, "/auth/")
