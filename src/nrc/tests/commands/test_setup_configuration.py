443d598a161ad243891fdb7486cd143e758543b2
nathanael <nathanael.gey@gmail.com>
2023-08-18 16:41:53 +0200

    Fixes #87 -- Test for setup configuration command

--- src/nrc/tests/commands/test_setup_configuration.py
+++ src/nrc/tests/commands/test_setup_configuration.py
@@ -1,50 +1,100 @@
 from django.core.management import call_command
 from django.test import TestCase
 
+from notifications_api_common.models import NotificationsConfig, Subscription
+
 from vng_api_common.authorizations.models import AuthorizationsConfig
 from vng_api_common.constants import ComponentTypes
 from vng_api_common.models import APICredential, JWTSecret
 
+from zgw_consumers.models.services import Service
+from zgw_consumers.constants import APITypes, AuthTypes
+
 
 class SetupConfigurationTests(TestCase):
     def test_setup_configuration(self):
-        ac_root = "https://open-zaak.utrecht.nl/autorisaties/api/v1/"
-        municipality = "Utrecht"
-        openzaak_to_notif_secret = "12345"
-        notif_to_openzaak_secret = "54321"
+        ac_root = "https://test.openzaak.nl/autorisaties/api/v1/"
+        notif_root = "http://127.0.0.1:8000/"
+        callback_url = "http://127.0.0.1:8000/api/v1/callbacks"
+        municipality = "MaykinMedia"
+        notif_client_id = "open-notificaties"
+        notif_secret_key = "foobar"
+        openzaak_client_id = "open-zaak"
+        openzaak_secret_key = "foobar"
 
         call_command(
-            "setup_configuration",
+            "new_setup_configuration",
             ac_root,
             municipality,
-            openzaak_to_notif_secret,
-            notif_to_openzaak_secret,
+            notif_root,
+            callback_url,
+            notif_client_id,
+            openzaak_client_id,
+            notif_secret_key,
+            openzaak_secret_key,
         )
 
+        # Test Step 1
         ac_config = AuthorizationsConfig.get_solo()
         self.assertEqual(ac_config.api_root, ac_root)
         self.assertEqual(ac_config.component, ComponentTypes.nrc)
 
-        api_credential = APICredential.objects.get()
-        self.assertEqual(api_credential.api_root, ac_root)
+        # Test Step 2
+        service = Service.objects.get()
+        self.assertEqual(service.api_type, APITypes.nrc)
+        self.assertEqual(service.auth_type, AuthTypes.zgw)
+        self.assertEqual(service.api_root, notif_root)
+
+        notif_config = NotificationsConfig.objects.get()
+        self.assertEqual(notif_config.notifications_api_service, service)
+
+        sub = Subscription.objects.get()
+        self.assertEqual(sub.callback_url, callback_url)
+        self.assertEqual(sub.client_id, notif_client_id)
+        self.assertEqual(sub.channels, ["autorisaties"])
+
+        # Test Step 3
+        api_credential_notif = APICredential.objects.get(api_root=ac_root)
+        self.assertEqual(api_credential_notif.api_root, ac_root)
         self.assertEqual(
-            api_credential.label,
-            f"Open Zaak {municipality}",
+            api_credential_notif.label,
+            f"Open Zaak Autorisaties API {municipality}",
         )
+        self.assertEqual(api_credential_notif.client_id, "open-notificaties")
+        self.assertEqual(api_credential_notif.secret, notif_secret_key)
+        self.assertEqual(api_credential_notif.user_id, "open-notificaties")
         self.assertEqual(
-            api_credential.client_id, f"open-notificaties-{municipality.lower()}"
-        )
-        self.assertEqual(api_credential.secret, notif_to_openzaak_secret)
-        self.assertEqual(
-            api_credential.user_id, f"open-notificaties-{municipality.lower()}"
-        )
-        self.assertEqual(
-            api_credential.user_representation, f"Open Notificaties {municipality}"
+            api_credential_notif.user_representation,
+            f"Open Notificaties {municipality}",
         )
 
-        notif_api_jwtsecret_ac = JWTSecret.objects.get()
+        # Test Step 4
+        api_credential_opencase = APICredential.objects.get(api_root=notif_root)
+        self.assertEqual(api_credential_opencase.api_root, notif_root)
+        self.assertEqual(
+            api_credential_opencase.label,
+            f"Own API {municipality}",
+        )
+        self.assertEqual(api_credential_opencase.client_id, "open-notificaties")
+        self.assertEqual(api_credential_opencase.secret, notif_secret_key)
+        self.assertEqual(api_credential_opencase.user_id, "open-notificaties")
+        self.assertEqual(
+            api_credential_opencase.user_representation,
+            f"Open Notificaties {municipality}",
+        )
+
+        # Test Step 5
+        notif_api_jwtsecret_ac = JWTSecret.objects.get(identifier=openzaak_client_id)
         self.assertEqual(
             notif_api_jwtsecret_ac.identifier,
-            f"open-zaak-backend-{municipality.lower()}",
+            "open-zaak",
         )
-        self.assertEqual(notif_api_jwtsecret_ac.secret, openzaak_to_notif_secret)
+        self.assertEqual(notif_api_jwtsecret_ac.secret, openzaak_secret_key)
+
+        # Test Step 6
+        notif_api_jwtsecret_notif = JWTSecret.objects.get(identifier=notif_client_id)
+        self.assertEqual(
+            notif_api_jwtsecret_notif.identifier,
+            "open-notificaties",
+        )
+        self.assertEqual(notif_api_jwtsecret_notif.secret, notif_secret_key)
