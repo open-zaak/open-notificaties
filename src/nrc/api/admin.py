c80140d0bad5e53e4b3983ec06948f3bbfa550a2
Conor Holden <conor@maykinmedia.nl>
2024-04-26 15:12:56 +0200

    :sparkles:[open-zaak/open-zaak#1626] add generated JWT to admin list display

---
+++ src/nrc/api/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
+
+from vng_api_common.models import JWTSecret
+from zds_client import ClientAuth
+
+admin.site.unregister(JWTSecret)
+
+
+@admin.register(JWTSecret)
+class JWTSecretAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "identifier",
+        "get_jwt",
+    )
+
+    @admin.display(description="jwt")
+    def get_jwt(self, obj):
+        if obj.identifier and obj.secret:
+            auth = ClientAuth(obj.identifier, obj.secret)
+            jwt = auth.credentials()["Authorization"]
+            return format_html(
+                '<code class="copy-action jwt" data-copy-value="{val}">{val}</code><p>{hint}</p>',
+                val=jwt,
+                hint=_("Gebruik het JWT-token nooit direct in een applicatie."),
+            )
+        return ""
