0f35994700514f7c534f0fb5525808401dd65e42
Sonny Bakker <sonny871@hotmail.com>
2024-11-12 14:15:32 +0100

    [#66] reuse `generate_jwt_auth` for JWTSecret admin

--- src/nrc/api/admin.py
+++ src/nrc/api/admin.py
@@ -3,7 +3,7 @@ from django.utils.html import format_html
 from django.utils.translation import gettext_lazy as _
 
 from vng_api_common.models import JWTSecret
-from zds_client import ClientAuth
+from vng_api_common.authorizations.utils import generate_jwt
 
 admin.site.unregister(JWTSecret)
 
@@ -16,13 +16,16 @@ class JWTSecretAdmin(admin.ModelAdmin):
     readonly_fields = ("get_jwt",)
 
     @admin.display(description="jwt")
-    def get_jwt(self, obj):
+    def get_jwt(self, obj: JWTSecret):
         if obj.identifier and obj.secret:
-            auth = ClientAuth(obj.identifier, obj.secret)
-            jwt = auth.credentials()["Authorization"]
             return format_html(
                 '<code class="jwt">{val}</code><p>{hint}</p>',
-                val=jwt,
+                val=generate_jwt(
+                    object.identifier,
+                    object.secret,
+                    "",
+                    ""
+                ),
                 hint=_("Gebruik het JWT-token nooit direct in een applicatie."),
             )
         return ""
