ff9f8142a677234cc5bc097e4dd4ecb66e3b220e
Daniel Mursa <daniel@maykinmedia.nl>
2025-02-03 16:11:40 +0100

    [maykinmedia/open-api-framework#100] Add quick-start.yml

--- bin/setup_configuration.sh
+++ bin/setup_configuration.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# setup initial configuration using environment variables
+# setup initial configuration using a yaml file
 # Run this script from the root of the repository
 
-#set -e
+set -e
 
-src/manage.py migrate
-src/manage.py setup_configuration --yaml-file setup_configuration/data.yaml
+if [[ "${RUN_SETUP_CONFIG,,}" =~ ^(true|1|yes)$ ]]; then
+    # wait for required services
+    /wait_for_db.sh
+
+    src/manage.py migrate
+    src/manage.py setup_configuration --yaml-file setup_configuration/data.yaml
+fi
