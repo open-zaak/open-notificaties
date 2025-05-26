9316a51d230673107d616ee49969ce7cd912dc41
Steven Bal <steven@maykinmedia.nl>
2025-05-26 14:01:01 +0200

    :bookmark: [#285] Release version 1.10.0

--- docs/conf.py
+++ docs/conf.py
@@ -112,6 +112,10 @@ extlinks = {
         "https://github.com/maykinmedia/open-api-framework/issues/%s",
         "#%s",
     ),
+    "open-api-workflows": (
+        "https://github.com/maykinmedia/open-api-workflows/issues/%s",
+        "#%s",
+    ),
     "maykin-charts": (
         "https://github.com/maykinmedia/charts/issues/%s",
         "#%s",
