816c41ac8b04e4162b70cf2cc0f50be7e56a9f2d
Steven Bal <steven@maykinmedia.nl>
2026-01-19 11:22:32 +0100

    :sparkles: [#356] Allow configuring cloud event filters via setup config
    for backwards compatibility, both a list of type substrings and a list with more complex filters are supported

--- src/nrc/setup_configuration/models.py
+++ src/nrc/setup_configuration/models.py
@@ -65,15 +65,37 @@ class KanalenFilterConfigurationModel(ConfigurationModel):
     naam: str = Field(description="The name of the channel", examples=["zaken"])
 
 
+class CloudEventFilterConfigurationModel(ConfigurationModel):
+    filters: dict[str, str] = Field(
+        description="Key value mapping based on which cloud events will be filtered",
+        default_factory=dict,
+        examples=[
+            {
+                "zaaktype": "https://openzaak.local/catalogi/api/v1/zaaktypen/d109cd8a-fe7b-4eb2-8cab-766712a4a267"
+            }
+        ],
+    )
+    type_substring: str = Field(
+        description="cloudevent type substrings that the subscription will watch for",
+        examples=["nl.overheid.zaken"],
+    )
+
+
 class AbonnementConfigurationItem(ConfigurationModel):
     uuid: UUID4 = Field(description="The UUID for this Abonnement.")
     kanalen: Optional[list[KanalenFilterConfigurationModel]] = Field(
         default=[], description="A list of channels which are subscribed to"
     )
-    cloudevent_filters: Optional[list[str]] = Field(
+    # XXX: deprecate list of strings option in next major version
+    cloudevent_filters: Optional[
+        list[CloudEventFilterConfigurationModel] | list[str]
+    ] = Field(
         default=[],
-        description="A list of cloudevent type substrings that the subscription will watch for",
-        examples=[["nl.overheid.zaken"]],
+        description=(
+            "A list of cloudevent filters that the subscription will watch for. "
+            "This supports either a list of cloud event type substrings, or a list of "
+            "type substrings with additional filters."
+        ),
     )
 
     class Meta:
