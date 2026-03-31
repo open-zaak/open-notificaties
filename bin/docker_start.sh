765d65e8b4b205a957ee7e4d9bb41f84c1cdc6b5
OlhaZahoruiko <olha@maykinmedia.nl>
2026-03-31 11:44:04 +0200

    ⚡ [maykinmedia/open-api-framework#211] Optimize memory usage (max-requests, flower)

--- bin/docker_start.sh
+++ bin/docker_start.sh
@@ -43,6 +43,9 @@ if [ -n "${OPENNOTIFICATIES_SUPERUSER_USERNAME}" ]; then
     unset OPENNOTIFICATIES_SUPERUSER_USERNAME OPENNOTIFICATIES_SUPERUSER_EMAIL DJANGO_SUPERUSER_PASSWORD
 fi
 
+# Periodically recycle workers - recover memory in the event of memory leaks
+export UWSGI_MAX_REQUESTS=${UWSGI_MAX_REQUESTS:-1000}
+
 # Start server
 >&2 echo "Starting server"
 exec uwsgi \
